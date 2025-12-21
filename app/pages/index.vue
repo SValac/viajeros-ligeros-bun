7f1e2bdd4a065208dcf446f193a87977660d66c8
Valac <shiro.valac@gmail.com>
2025-12-21 01:33:10 -0600

    feat: restructure app layout and add dashboard components

---
+++ app/pages/index.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main class="flex flex-1">
+    <UDashboardPanel
+      id="inbox-1"
+      resizable
+    >
+      <template #header>
+        <UDashboardNavbar title="Inbox">
+          <template #leading>
+            <UDashboardSidebarCollapse />
+          </template>
+        </UDashboardNavbar>
+      </template>
+
+      <template #body>
+        <div class="h-full bg-amber-200" />
+      </template>
+    </UDashboardPanel>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
