a0aaf82b7164c5b37e77f8150a3fe5a6d65528f9
SValac <shiro.valac@gmail.com>
2026-09-25 16:24:10 -0600

    chore(sidebar): remove unused search button
    It was a leftover from the dashboard template with no search behind it.

--- app/components/the-sidebar.vue
+++ app/components/the-sidebar.vue
@@ -98,22 +98,6 @@ const items = computed<NavigationMenuItem[][]>(() => [[{
     </template>
 
     <template #default="{ collapsed }">
-      <UButton
-        :label="collapsed ? undefined : 'Search...'"
-        icon="i-lucide-search"
-        color="neutral"
-        variant="outline"
-        block
-        :square="collapsed"
-      >
-        <template v-if="!collapsed" #trailing>
-          <div class="flex items-center gap-0.5 ms-auto">
-            <UKbd value="meta" variant="subtle" />
-            <UKbd value="K" variant="subtle" />
-          </div>
-        </template>
-      </UButton>
-
       <UNavigationMenu
         :collapsed="collapsed"
         :items="items[0]"
